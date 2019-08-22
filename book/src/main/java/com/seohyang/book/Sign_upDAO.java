a8225960b5567603ebaab7d73566bf8c79a8e67e
yoonhyunwoo5135 <49705388+yoonhyunwoo5135@users.noreply.github.com>
2019-08-22 15:00:08 +0900

    Add files via upload

--- book/src/main/java/com/seohyang/book/Sign_upDAO.java
+++ book/src/main/java/com/seohyang/book/Sign_upDAO.java
@@ -13,5 +13,14 @@ public class Sign_upDAO {
 	public void insert(Sign_upDTO sign_upDTO){
 		my.insert("signDAO.insert", sign_upDTO);
 	}
-	
+	public String select(String id) {
+		String result = my.selectOne("signDAO.select", id);
+		if(result == null || result.trim().equals("")) {
+			result = "1";
+			return result;
+		}
+		else {
+			return my.selectOne("signDAO.select", id);			
+		}
+	}
 }
